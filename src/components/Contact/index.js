bc4d23831cc2ec7240db855fff0486ac0a1a83bc
melaniemdm <melanieborja.pro@gmail.com>
2022-05-19 10:53:38 +0200

    feature(js & css) formulaire contact

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -3,23 +3,17 @@ import './style.scss';
 function Contact(){
     return(<div className="containerContact">
 
-<div class="container">
+<div class="containerForm">
   <h1>Formulaire de contact</h1>
  
-    <label for="fname">Nom et prénom</label>
-    <input type="text" id="fname" name="firstname" placeholder="Votre nom et prénom"/>
-
-    <label for="sujet">Sujet</label>
-    <input type="text" id="sujet" name="sujet" placeholder="L'objet de votre message"/>
-
-    <label for="emailAddress">Email</label>
-    <input id="emailAddress" type="email" name="email" placeholder="Votre email"/>
-
-
-    <label for="subject">Message</label>
-    <textarea id="subject" name="subject" placeholder="Votre message" ></textarea>
-
-    <input type="submit" value="Envoyer"/>
+  <iframe
+  src="https://tally.so/embed/npbr9y?alignLeft=1&hideTitle=1&transparentBackground=1"
+  
+  frameborder="0"
+  marginheight="0"
+  marginwidth="0"
+  title="Nous contacter">
+</iframe>
 
 </div>
 
