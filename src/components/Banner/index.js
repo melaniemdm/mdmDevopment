fa44420a4d0501e529a59ce013f6b90f568d3f64
melaniemdm <melanieborja.pro@gmail.com>
2022-06-24 13:36:12 +0200

    fix(js&css) refacto, bannerTxt

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,10 +4,16 @@ import imgBanner from '../../assets/banner.jpg';
 
 function Banner(){
 return(
-    <div>
+  <div>
+    <div className="bannerTxt">
         <div className="containerAccroche"> Développez votre visiblité !</div>
         <div className="demandedevisTxt">Demandez un devis gratuit</div>
-      <div className="containerBanner">  <img src={imgBanner} alt="paysage" class="banner"/></div></div>
+        </div>
+      <div className="containerBannerImg">  <img src={imgBanner} alt="paysage" /></div>
+      
+      
+      
+      </div>
 )
 
 }
