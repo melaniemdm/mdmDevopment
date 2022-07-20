76415b8f7c83b02996c1695d011ef8959e80c427
melaniemdm <melanieborja.pro@gmail.com>
2022-07-20 22:09:16 +0200

    fix(css & js) banner color

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -6,7 +6,7 @@ function Banner(){
 return(
   <div>
     <div className="bannerTxt">
-        <div className="containerAccroche"> Développez votre visiblité !</div>
+        <div className="containerAccroche"> Développez votre visibilité !</div>
         <div className="demandedevisTxt">Demandez un devis gratuit</div>
         </div>
       <div className="containerBannerImg">  <img src={imgBanner} alt="paysage" /></div>
