0223434ac1a6a9b7bb069d051f09b8ace06eecc4
melaniemdm <melanieborja.pro@gmail.com>
2022-07-25 11:11:37 +0200

    fix(cqss) all dimensionnement

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,24 +1,26 @@
 import './style.scss';
 
 function Contact(){
-  return(<div className="containerContact" id="contactMe">
-
-    <div className="containerForm">
-      <h1>Formulaire de contact</h1>
+  return(
+    <section id="contactMe">  
+      <div className="containerContact" >
  
-      <iframe
-        src="https://tally.so/embed/npbr9y?alignLeft=1&hideTitle=1&transparentBackground=1"
+        <div className="containerForm">
+          <h1>Formulaire de contact</h1>
+ 
+          <iframe
+            src="https://tally.so/embed/npbr9y?alignLeft=1&hideTitle=1&transparentBackground=1"
   
-        frameBorder="0"
-        marginHeight="0"
-        marginWidth="0"
-        title="Nous contacter">
-      </iframe>
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+            title="Nous contacter">
+          </iframe>
 
-    </div>
+        </div>
 
 
-  </div>
+      </div></section>
   );
 }
 export default Contact;
