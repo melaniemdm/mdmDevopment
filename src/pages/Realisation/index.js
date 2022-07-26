e9f05eb4bd12eec450e7afe51d77347533e080fd
melaniemdm <melanieborja.pro@gmail.com>
2022-07-26 11:22:45 +0200

    fix(css & js) lien git

--- src/pages/Realisation/index.js
+++ src/pages/Realisation/index.js
@@ -5,22 +5,22 @@ function Realisation(){
 
     {listeDesProjets.map((item,index)=>
       <section className="cardRealisation" key={index}>
-      
-        <div className="cardRealisation-thumb" >
-          <a href={item.lien}><img src={item.picture}className="imgProjet" alt="img" /></a>
-        </div>
+        <a href={item.lien}>
+          <div className="cardRealisation-thumb" >
+            <img src={item.picture}className="imgProjet" alt="img" />
+          </div>
    
-        <div className="cardRealisation-body">
-          <h2 className="cardRealisation-title">{item.title}</h2>
+          <div className="cardRealisation-body">
+            <h2 className="cardRealisation-title">{item.title}</h2>
        
-          <p className="cardRealisation-description">{item.describe}
-          </p>
-        </div>
+            <p className="cardRealisation-description">{item.describe}
+            </p>
+          </div>
       
         
         
        
-   
+        </a>
       </section>
     )}
 
