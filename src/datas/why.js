1ba8c211fa1d76f034dc678ea88faa0a8ff940ac
melaniemdm <melanieborja.pro@gmail.com>
2022-09-02 11:56:02 +0200

    typo(why)

--- src/datas/why.js
+++ src/datas/why.js
@@ -3,19 +3,19 @@ export const why=[
     nameWhy:'Augmenter votre visiblité',
     pictureWhy:process.env.PUBLIC_URL + '/assets/voir.png',
     titleWhy:'Augmenter votre visiblité sur le web... ',
-    detailWhy:'Augmenter votre visiblité sur le web pour promouvoir votre société auprès de vos prospects ains que de vos clients. Gérer votre image afin de mettre société en valeur sur mesure.'
+    detailWhy:'Augmenter votre visiblité sur le web pour promouvoir votre société auprès de vos prospects ains que de vos clients. Gérer votre image afin de mettre votre société en valeur sur mesure.'
   },
   {
     nameWhy:'Elargir votre base',
     pictureWhy:process.env.PUBLIC_URL + '/assets/elargirBaseClient.png',
     titleWhy:'Elargir votre base de clientèle',
-    detailWhy:'Toucher de nouveau prospect via le web et faliciter la prise de contact via un formulaire directement sur votre site vitrine. '
+    detailWhy:'Toucher de nouveaux prospects via le web et faciliter la prise de contact via un formulaire directement sur votre site vitrine. '
   },
   {
     nameWhy:'Rassurer vos clients',
     pictureWhy:process.env.PUBLIC_URL + '/assets/prospect.png',
     titleWhy:'Rassurer vos clients et vos prospects',
-    detailWhy:'Votre site vitrine est le miroir de votre activité et la manière dont les visiteurs vont vous percevoir. Démarquez vous en réalisant votre site vitrine sur mesure.'
+    detailWhy:'Votre site vitrine est le miroir de votre activité et la manière dont les visiteurs vont vous percevoir. Démarquez-vous en réalisant votre site vitrine sur mesure.'
   },
   {
     nameWhy:'Présenter votre activité',
