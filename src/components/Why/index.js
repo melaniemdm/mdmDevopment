0d1cae571cfdefe8d9dbbe662fabc31ca238965a
melaniemdm <melanieborja.pro@gmail.com>
2023-01-20 10:13:43 +0100

    fix(css & js) component Why

--- src/components/Why/index.js
+++ src/components/Why/index.js
@@ -1,58 +1,29 @@
 import './style.scss';
-import { why} from '../../datas/why';
+import {why} from '../../datas/why';
 
 function WhyWebsite(){
-  return(<section>
-    <div className="containerWhy" id="why">
-      <div className="title" > Pourquoi un site vitrine?</div>
-      <div className="containerWhyCard">
-        {why.map((item,index)=>  <div className="card" key={index} >
-          <div className="face face1">
-            <div className="content">
-                  
-              <h3>{item.titleWhy}</h3>
-            </div>
-          </div>
-          <div className="face face2">
-            <div className="content">
-              <p>{item.detailWhy}</p>
-                       
-            </div>
+  
+  return( <section className="sectionWhy">
+    <div className="titleWhy" > Pourquoi un site vitrine?</div>
 
-          </div>
-        </div>)
-        }
+    {why.map((item, index)=>   <div className="cardWhy-basic" key={index}>
+      <div className="cardWhy-header headerWhy-basic">
+        <h1 className='colorTitleWhy'>{item.titleWhy}</h1>
       </div>
-
-
+      <div className="cardWhy-bodyWhy">
+        
+        <div className="cardWhy-element-hidden-basic">
+          <ul className="cardWhy-element-containerWhy">
+            <li className="cardWhy-element">{item.detailWhy}</li>
+             
+          </ul>
+          
+        </div>
+      </div>
     </div>
-
+    )}
   </section>
-
-
-
-  //       <div className="containerWhy" id="why">
-  //          <div className="titleWhy"> Pourquoi un site vitrine?</div>
-  //       <div className="cards-listWhy">
-
-  //   {why.map((item,index)=><div className="cardWhy" key={index} >  
-  //   <img src={item.pictureWhy} alt={item.nameWhy} className="whyImg"/>
-  //   <div className="detailWhyCard">
-
-  //   <div className='titleWhyDetail'>{item.titleWhy}</div>
-  //   <div className='detailWhy'>{item.detailWhy}</div>
- 
-  
-  //    </div>
-  //    </div>)
-
-  //   }
-  
-  
-  // </div>
-  // </div>
-
-
+      
     
   );
 }
