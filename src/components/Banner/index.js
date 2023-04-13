d2453ebae4d5601b1184270f051b7435804acb9e
melaniemdm <melanieborja.pro@gmail.com>
2023-04-13 10:48:06 +0200

    fix(css et js) responsive

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -4,10 +4,9 @@ function Banner(){
   return(
 
     <div className="bannerTxt">
-      <div className="separator"></div>
       <div className="containerAccroche"> Développez votre visibilité !</div>
       <div className="demandedevisTxt">Demandez un devis gratuit</div>
-      <div className="separator"></div>
+     
     </div>
  
   );
